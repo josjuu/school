fa232309cc26d6449ac5b03a9b73740c00ed8bf1
Jos Mutter <jos.mutter@student.graafschapcollege.nl>
2018-04-19 19:57:31 +0200

    Updated project

---
+++ School.WEB/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using School.DAL;
+using School.DAL.Models;
+using School.WEB.Models;
+
+namespace School.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student();
+            student.Firstname = "Jos";
+            student.Lastname = "Mutter";
+
+            using (var db = new SchoolContext())
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            using (var db = new SchoolContext())
+            {
+                var student = db.Students.SingleOrDefault(s => s.Firstname == "Jos" && s.Lastname == "Mutter");
+
+                if (student != null)
+                {
+                    db.Students.Remove(student);
+                    db.SaveChanges();
+                }
+            }
+
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ School.WEB/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace School.WEB.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
