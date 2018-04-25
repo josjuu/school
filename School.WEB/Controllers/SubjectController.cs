98fbde95bcdfd40399ae3e831a4d1ce2226c2745
Jos Mutter <jos.mutter@student.graafschapcollege.nl>
2018-04-25 19:54:02 +0200

    Updated project

--- School.WEB/Controllers/SubjectController.cs
+++ School.WEB/Controllers/SubjectController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using School.DAL;
+using School.DAL.Models;
 using School.WEB.Viewmodels;
 
 namespace School.WEB.Controllers
@@ -8,6 +10,35 @@ namespace School.WEB.Controllers
         public IActionResult Index()
         {
             return View("Index", new SubjectIndex());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View("Add", new SubjectAdd());
+        }
+
+        [HttpPost]
+        public IActionResult Add(SubjectAdd subjectAdd)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Add", subjectAdd);
+            }
+
+            using (var db = new SchoolContext())
+            {
+                var subject = new Subject
+                {
+                    SubjectName = subjectAdd.Name,
+                    TeacherId = subjectAdd.TeacherId
+                };
+
+                db.Subjects.Add(subject);
+                db.SaveChanges();
+            }
+
+            return RedirectToActionPermanent("Index", "Subject");
         }
     }
 }
